3f3d958fd44cac00227ee49c8e9d81617ddb8f73
Jauder Ho <jauderho@users.noreply.github.com>
2021-11-08 09:34:49 +0000

    Add GHA based update mechanism
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

---
+++ scripts/updateDockerImages.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Script to check and start new image builds if the source repos have new releases
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+# $PAT variable needs to be passed into the script as an env variable
+# PAT is only used to avoid API rate limits
+
+REPO=( 
+	"FiloSottile/age" \
+	"nakabonne/ali" \
+	"ansible/ansible" \
+	"jauderho/bl3auto" \
+	"jauderho/cf-warp" \
+	"cloudflare/cloudflared" \
+	"coredns/coredns" \
+	"wagoodman/dive" \
+	"StackExchange/dnscontrol" \
+	"DNSCrypt/dnscrypt-proxy" \
+	"cloudskiff/driftctl" \
+	"osrg/gobgp" \
+	"kffl/gocannon" \
+	"juanfont/headscale" \
+	"go-acme/lego" \
+	"slackhq/nebula" \
+	"cube2222/octosql" \
+	"aramperes/onetun" \
+	"cilium/pwru" \
+	"authzed/spicedb" \
+	"jtesta/ssh-audit" \
+	"nabla-c0d3/sslyze" \
+	"tailscale/tailscale" \
+	"hashicorp/terraform" \
+	"ytdl-org/youtube-dl" \
+	"yt-dlp/yt-dlp" \
+)
+
+# setup git
+git config user.name "updatebot"
+git config user.email "jauderho+update@users.noreply.github.com"
+
+# 
+# Pull in the latest version from GitHub and if there is a newer version, update GitHub Actions to trigger a new build
+# Right noew it's just a string compare
+for i in "${REPO[@]}"
+do
+
+	prog=$(echo "$i" | sed -e "s/.*\///")
+	dver=$(grep "BUILD_VERSION:" ".github/workflows/${prog}.yml" | cut -d \" -f 2)
+
+	if [ "$i" != "ansible/ansible" ]; then
+		#rver=$(curl -sL -u "$PAT" "https://api.github.com/repos/${i}/releases/latest" | grep tag_name | head -1 | cut -d \" -f 4)
+		rver=$(curl -sL -u "$PAT" "https://api.github.com/repos/${i}/releases/latest" | jq -r '.tag_name')
+		#rver=$(curl -sL "https://api.github.com/repos/${i}/releases/latest" | grep tag_name | head -1 | cut -d \" -f 4)
+		#rver="2021.02.04.1"
+
+	else
+
+		# special case for ansible
+		# ansible is a pain and does not put the release tag in the same repo (ansible/ansible) but ansible-community/ansible-build-data instead
+		rver=$(curl -sL https://pypi.org/pypi/ansible/json | jq -r '.info.version')
+	fi
+
+	echo "Checking repo ... $prog"
+	echo 
+	echo "    Dockerfile version is $dver"
+	echo "    Repo version is	  $rver"
+	echo
+
+	if [ "$dver" != "$rver" ]; then
+		echo "Updating to ${rver} ..." 
+
+		sed -i -e "s/$dver/$rver/" ".github/workflows/${prog}.yml" && \
+		git add ".github/workflows/${prog}.yml" && \
+		git commit -m "Updated ${prog} to ${rver}"
+
+	else
+		echo "No update needed ..."
+	fi
+
+	echo
+	echo
+	echo
+done
+
+# Make it so
+git push
