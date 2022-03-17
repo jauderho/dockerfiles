6a133e1facdadf040dbefe066adcc5e11eec2381
Jauder Ho <jauderho@users.noreply.github.com>
2022-03-17 01:03:56 -0700

    Update entrypoint.sh

--- pia-wg/entrypoint.sh
+++ pia-wg/entrypoint.sh
@@ -4,8 +4,8 @@ set -Eeuo pipefail
 # Generate WG config and show QR code
 python3 pia-wg/generate-config.py
 echo
-cat PIA*conf
+cat PIA.conf
 echo
-cat PIA*conf | qrencode -t ansiutf8 
+cat PIA.conf | qrencode -t ansiutf8 
 echo
 
