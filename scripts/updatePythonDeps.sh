51fe35c71c5dfb9d2ad037a5324cfa243c8f2f0a
Jauder Ho <jauderho@users.noreply.github.com>
2022-05-20 12:20:39 -0700

    Update updatePythonDeps.sh

--- scripts/updatePythonDeps.sh
+++ scripts/updatePythonDeps.sh
@@ -21,6 +21,7 @@ cd "$1"
 echo "Updating $1 ..."
 echo
 
+python -m pip install -U setuptools
 #pipenv --python 3.10 lock && pipenv --python 3.10 lock -r > requirements.txt
 #pipenv --python 3.9 lock && pipenv --python 3.9 lock -r > requirements.txt
 pipenv --python 3.9 lock
