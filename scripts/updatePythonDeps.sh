1a8cdb0d1da42da71b8c9f05a9619541f2e1947c
Jauder Ho <jauderho@users.noreply.github.com>
2022-11-08 12:18:28 -0800

    Update updatePythonDeps.sh
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- scripts/updatePythonDeps.sh
+++ scripts/updatePythonDeps.sh
@@ -14,21 +14,21 @@ git config pull.rebase false
 #PATH="$HOME/.local/bin:$PATH"
 #pipenv install --python 3.9
 #pipenv shell
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.11
-#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv 
+#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.11
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv 
 
 cd "$1"
 
 echo "Updating $1 ..."
 echo
 
-# for python3.11 and ubuntu 22.04
+# for python3.10 and ubuntu 22.04
 #python -m pip install -U pip
 #python -m pip install -U setuptools
 python3 -m pip install --no-cache-dir --upgrade pip
 pip install --upgrade --user pipenv
-#pipenv lock && pipenv requirements > requirements.txt
-pipenv --python 3.11 lock && pipenv --python 3.11 requirements > requirements.txt
+pipenv lock && pipenv requirements > requirements.txt
+#pipenv --python 3.11 lock && pipenv --python 3.11 requirements > requirements.txt
 
 git add Pipfile Pipfile.lock requirements.txt && \
 git commit -s -m "Update requirements for $1 ..." && \
