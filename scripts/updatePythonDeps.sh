cc78d11f9d0bbc326a4c2e61e1260d380a978337
Jauder Ho <jauderho@users.noreply.github.com>
2022-11-08 12:07:55 -0800

    Update updatePythonDeps.sh
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- scripts/updatePythonDeps.sh
+++ scripts/updatePythonDeps.sh
@@ -14,27 +14,21 @@ git config pull.rebase false
 #PATH="$HOME/.local/bin:$PATH"
 #pipenv install --python 3.9
 #pipenv shell
-#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.10
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.11
+#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv 
 
 cd "$1"
 
 echo "Updating $1 ..."
 echo
 
-# for python3.10 and ubuntu 22.04
+# for python3.11 and ubuntu 22.04
 #python -m pip install -U pip
 #python -m pip install -U setuptools
 python3 -m pip install --no-cache-dir --upgrade pip
 pip install --upgrade --user pipenv
-pipenv lock && pipenv requirements > requirements.txt
-#pipenv --python 3.10 lock && pipenv --python 3.10 requirements > requirements.txt
-
-# stick with python3.9 and ubuntu 20.04 for now
-#pipenv --python 3.9 lock && pipenv --python 3.9 lock -r > requirements.txt
-#pipenv --python 3.9 lock
-#pipenv --python 3.9 lock -r > requirements.txt
-#pipenv lock && pipenv lock -r > requirements.txt
+#pipenv lock && pipenv requirements > requirements.txt
+pipenv --python 3.11 lock && pipenv --python 3.11 requirements > requirements.txt
 
 git add Pipfile Pipfile.lock requirements.txt && \
 git commit -s -m "Update requirements for $1 ..." && \
