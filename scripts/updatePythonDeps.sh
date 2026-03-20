1050835bbe5a24749c30a6cc3a51bf967f6db7f4
Jauder Ho <jauderho@users.noreply.github.com>
2026-03-20 21:15:43 +0000

    Cleanup
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- scripts/updatePythonDeps.sh
+++ scripts/updatePythonDeps.sh
@@ -10,29 +10,14 @@ IFS=$'\n\t'
 #git config --local user.email "jauderho-bot@users.noreply.github.com"
 #git config --local pull.rebase false
 
-# setup pipenv and python
-#PATH="$HOME/.local/bin:$PATH"
-#pipenv install --python 3.12
-#pipenv shell
-###DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.12
-#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv 
-
 cd "$1"
 
 echo "Updating $1 ..."
 echo
 
-# for python3.10 and ubuntu 22.04
-#python -m pip install -U pip
-#python -m pip install -U setuptools
-###python3.12 -m pip install --no-cache-dir --upgrade pip --break-system-packages
-###python3.12 -m pip install --no-cache-dir --upgrade --user pipenv certifi wheel setuptools packaging --break-system-packages
-#pipenv lock && pipenv requirements > requirements.txt
-###pipenv --python 3.12 lock && pipenv --python 3.12 requirements --exclude-markers > requirements.txt
-
+# Build dependencies
 uv lock -U && uv pip compile pyproject.toml --no-annotate > requirements.txt
 
-###git add Pipfile Pipfile.lock requirements.txt && \
 git add pyproject.toml uv.lock requirements.txt && \
 git commit -S -s -m "Update requirements for $1 ..." && \
 git pull && \
